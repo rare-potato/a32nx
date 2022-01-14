f39216b0d228a117bb421e84fdbea467babff8c7
Saschl <sascharudolf46@gmail.com>
2022-01-15 00:56:55 +0100

    refactor(pfd): move to MSFS avionics framework #6543

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,18 @@
 
 set -ex
 
+cd msfs-avionics-mirror/src/msfstypes
+npm pack
+cd ../sdk
+rm -rf node_modules
+rm -rf build
+npm install
+npm run build
+cp package.json build/
+cd build
+npm pack
+
+cd /external
 rm -f package-lock.json
 rm -rf node_modules
 npm install --no-optional
